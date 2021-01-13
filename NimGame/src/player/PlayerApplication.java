6306f1962a634acb3b3f02676c5e1b7aa5dd3a04
Hisui45 <jadelocke85@gmail.com>
2021-01-13 16:27:48 -0500

    Reorganizing Project Structure pt 2

---
+++ NimGame/src/player/PlayerApplication.java
@@ -0,0 +1,14 @@
+package player;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PlayerApplication extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		
+	}
+
+}
