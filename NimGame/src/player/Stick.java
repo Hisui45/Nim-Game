88338da0e18a9c179de5a356bc8779f136b0d6c6
Jade Locke <jadelocke85@gmail.com>
2021-01-25 22:28:14 -0500

    Graphical interface (#4)
    * -Completed First GameView design
    -Methods for userName and firstPlayer
    
    * Start Of Development of Game Play
    
    -Picking Starting Number Of Sticks
    -Receiving Stick Picks

--- NimGame/src/player/Stick.java
+++ NimGame/src/player/Stick.java
@@ -1,73 +1,26 @@
 package player;
 
-import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.net.MalformedURLException;
 
-import javafx.scene.shape.Line;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Stick extends Line2D {
-	
-	private double baseSize = 5; 
+public class Stick extends ImageView {
 	
 	public Stick() {
-//		this.setStartX(baseSize);
-//		this.setEndX(-baseSize);
-//		this.setStartY(-10);
-//		this.setEndY((baseSize*2)+50);
-//		this.setStrokeWidth(4);
-		this.setLine(30,60,75,75);
-	   // Line2D.Double arm1 =new Line2D.Double(30,60,75,75);
 	
-	}
-
-	@Override
-	public Rectangle2D getBounds2D() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Point2D getP1() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Point2D getP2() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double getX1() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getX2() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getY1() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getY2() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setLine(double x1, double y1, double x2, double y2) {
-		// TODO Auto-generated method stub
+		File file = new File(System.getProperty("user.dir")+"\\resources\\stick_image.png");
+		String localUrl = null;
+		try {
+			localUrl = file.toURI().toURL().toString();
+		} catch (MalformedURLException e) {
+			
+			e.printStackTrace();
+		}
+		Image logo = new Image(localUrl, 85, 85, false, false);
+	
+		this.setImage(logo);
 		
 	}
 }
